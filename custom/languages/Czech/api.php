2220a43e4d289940a5e62f1ac112654753b4f055
Quintupple <71530975+Quintupple@users.noreply.github.com>
2020-11-05 21:41:18 +0000

    adeded

---
+++ custom/languages/Czech/api.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr3
+ *
+ *  License: MIT
+ *
+ *  Czech Language - API
+ */
+$language = array(
+    // Errors
+    'unknown_error' => 'Neznámá chyba',
+    'invalid_api_key' => 'Neplatný API Key',
+    'invalid_language_file' => 'Neplatný soubor jazyk',
+    'invalid_api_method' => 'Neplatná API metoda',
+    'no_unique_site_id' => 'Není dostupné žádné ID stránky',
+    'unable_to_check_for_updates' => 'Nelze zjistit aktualizace',
+    'invalid_post_contents' => 'Neplatný obsah POST',
+    'invalid_get_contents' => 'Neplatný obsah GET',
+    'invalid_email_address' => 'Neplatný e-mail',
+    'invalid_username' => 'Neplatné jméno',
+    'invalid_uuid' => 'Neplatné UUID',
+    'email_already_exists' => 'Email již existuje',
+    'username_already_exists' => 'Jméno již existuje',
+    'uuid_already_exists' => 'UUID již existuje',
+    'unable_to_create_account' => 'Nelze vytvořit účet',
+    'unable_to_send_registration_email' => 'Nelze poslat registrační mail, kontaktuj administrátora',
+    'unable_to_find_user' => 'Nelze najít uživatele',
+    'unable_to_find_group' => 'Nelze najít skupinu',
+    'unable_to_update_group' => 'Nelze aktualizovat hráčovu skupinu',
+    'report_content_too_long' => 'Nahlášení nesmí být delší jak 255 znaků',
+    'you_must_register_to_report' => 'Musíte být registrován a ověřen, aby jsi mohl napsat nahlášení',
+    'you_have_been_banned_from_website' => 'Byl jste zabanován z této stránky',
+    'you_have_open_report_already' => 'Již jste otevřel nahlášení na tohoto hráče',
+    'unable_to_create_report' => 'Nelze vytvořit nahlášení',
+    'unable_to_update_username' => 'Nelze aktualizovat jméno',
+    'unable_to_update_server_info' => 'Nelze aktualizovat informace o serveru',
+    'invalid_server_id' => 'Neznámé ID serveru',
+    'invalid_code' => 'Byl vložen neznámý kód',
+    // Success messages
+    'finish_registration_link' => 'Prosím klikněte na tento odkaz pro dokončení registrace:',
+    'finish_registration_email' => 'Zkontrolujte email pro dokončení registrace.',
+    'group_updated' => 'Skupina úspěšně aktualizována',
+    'report_created' => 'Nahlášení úspěšně vytvořeno',
+    'new_private_message_from' => 'Nová soukromá zpráva od {x}', // Don't replace {x}
+    'username_updated' => 'Jméno úspěšně aktualizováno',
+    'server_info_updated' => 'Informace o serveru aktualizovány',
+	'account_validated' => 'Účet úspěšně ověřen.'
+);
---
+++ custom/languages/Portuguese/general.php
@@ -0,0 +1,153 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr6
+ *
+ *  License: MIT
+ *
+ *  Portuguese Language - General
+ *  Translation by Douglas Teles
+ *  Last Update: 24/04/2019
+ */
+$language = array(
+	/*
+	 *  General terms
+	 */ 
+	'submit' => 'Enviar',
+	'register' => 'Cadastrar',
+	'already_registered' => 'Já possui cadastro?',
+	'not_registered_yet' => 'Not registered yet?',
+	'sign_in' => 'Entrar',
+	'log_out' => 'Sair',
+	'profile' => 'Perfil',
+	'invalid_token' => 'Token inválido, tente novamente.',
+	'close' => 'Fechar',
+	'back' => 'Voltar',
+	'warning' => 'Atenção',
+	'date' => 'Data',
+	'search' => 'Procurar',
+	'edit' => 'Editar',
+	'next' => 'Próximo',
+	'previous' => 'Previous',
+	'confirm_cancel' => 'Tem certeza de que deseja cancelar?',
+	'confirm_delete' => 'Confirmar exclusão',
+	'confirm_deletion' => 'Tem certeza de que deseja excluir isso?',
+	'cancel' => 'Cancelar',
+	'delete' => 'Excluir',
+	'none' => 'Nenhum',
+	'view' => 'Visualizar',
+	'change' => 'Alterar',
+	'remove' => 'Remover',
+	'info' => 'Info',
+	'confirm' => 'Confirmar',
+	'actions' => 'Ações',
+	'alerts' => 'Alertas',
+	'view_alerts' => 'Visualizar alertas',
+	'no_alerts' => 'Sem alertas',
+	'x_new_alerts' => 'Você possuí {x} novos alertas.', // Don't replace {x}
+	'messages' => 'Mensagens',
+	'view_messages' => 'Visualizar mensagens',
+	'no_messages' => 'Sem mensagens',
+	'x_new_messages' => 'Você possuí {x} novas mensagens.', // Don't replace {x}
+	'loading' => 'Carregando...',
+	'report' => 'Relatório',
+	'page_loaded_in' => 'Página gerada em {x}s', // Don't replace {x}, time unit is seconds
+	'page_x' => 'Página {x}',
+	'browse' => 'Navegar',
+	'bypass' => 'Burlar',
+	'yes' => 'Sim',
+	'no' => 'Não',
+	'content' => 'Conteúdo',
+	'full_content' => 'Conteúdo Completo',
+	'url' => 'URL',
+	'success' => 'Sucesso',
+	'error' => 'Erro',
+	'are_you_sure' => 'Você tem certeza?',
+	'spoiler' => 'Spoiler',
+
+	/*
+	*  Contact form
+	*/
+	'contact' => 'Contato',
+	'message' => 'Mensagem',
+	'email_address' => 'Endereço de e-mail',
+	'contact_email_subject' => 'Novo formulário de contato',
+	'contact_message_failed' => 'Não foi possível a mensagem. Certifique-se de ter inserido uma mensagem entre 10 e 5000 caracteres.',
+	'contact_message_email' => 'O e-mail fornecido é inválido.',
+	'contact_message_sent' => 'Mensagem enviada com sucesso.',
+	'contact_message_limit' => 'Você só pode enviar uma mensagem por hora. Você pode enviar uma mensagem em {x} minutos.', // Don't replace {x}
+	
+	/* 
+	 *  Navbar
+	 */
+	'home' => 'Início',
+	'more' => 'Mais',
+	'status' => 'Status',
+	
+	/*
+	 *  Homepage
+	 */
+	'social' => 'Social',
+	
+	
+	/*
+	 *  Help dialogues
+	 */
+	'markdown_help' => 'Você pode formatar sua postagem usando o Markdown. Para mais informações, veja <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet\' target=\'_blank\'>aqui</a>.',
+
+	/*
+	*  Server query
+	*/
+	'online' => 'Online',
+	'offline' => 'Offline',
+	'player_count' => '{x}/{y}', // Don't replace {x} or {y}
+	'ping' => 'Ping: {x}', // Don't replace {x}
+	'version' => 'Versão: {x}', // Don't replace {x}
+	'currently_1_player_online' => 'No momento, há 1 jogador online.',
+	'currently_x_players_online' => 'Há <strong>{x}</strong> jogadores online.', // Don't replace {x}
+	'server_offline' => 'O servidor está atualmente offline.',
+	'connect_with_ip_x' => 'Junte-se aos nossos jogadores: {x}', // Don't replace {x}
+    'click_to_copy_tooltip' => 'Clique para copiar',
+    'ip' => 'IP',
+    'copied' => 'Copiado!',
+    'no_servers' => 'Não há servidores para exibir!',
+    'bungee_instance' => 'Este servidor é uma instância do Bungee.',
+    'no_players_online' => 'Não há jogadores online.',
+    'and_x_more' => 'e {x} mais', // Don't replace {x}
+
+	/*
+	 *  Cookie notice
+	 */
+	'cookie_notice' => 'Este site usa cookies para melhorar a sua experiência de navegação. Ao registar-se no nosso site ou fechar este aviso você concorda com a sua utilização.',
+	'cookie_agree' => 'Concordo',
+
+    /*
+     *  Widgets
+     */
+    'statistics' => 'Estatisticas',
+    'users_registered' => 'Usuários Registrados',
+    'latest_member' => 'Membro mais Recente',
+    'forum_statistics' => 'Estatísticas do Fórum',
+    'total_threads' => 'Total de Tópicos',
+    'total_posts' => 'Total de Postagens',
+    'online_staff' => 'Staff Online',
+    'no_online_staff' => 'Não há membros da staff online.',
+    'total_online_staff' => 'Staff online: {x}',
+    'online_users' => 'Usuários Online',
+    'no_online_users' => 'Não há usuários online.',
+    'total_online_users' => 'Usuários online: {x}',
+    'discord_widget_disabled' => 'O widget está desativado para o servidor Discord especificado. Por favor, vá para a aba \'Widget\' nas configurações do servidor Discord, e assegure-se de que o widget Discord esteja habilitado e que o ID esteja correto.',
+    'online_guests' => 'Online Guests',
+    'total_online' => 'Total Online', // this is Online Users + Online Guests
+
+    /*
+     *  Privacy policy
+     */
+    'privacy_policy' => 'Política de Privacidade',
+
+	/*
+	 *  Custom page redirect
+	 */
+	'redirecting_message' => 'Redirecionando...<br />Se você não for redirecionado automaticamente, clique <a href="{x}" rel="nofollow noopener">aqui</a>.' // Don't replace {x} which is the redirect URL
+);
